7f4742c935460d16f95777ff340e92c2dfbe8325
rebeccaruva <rebeccaruva@newschool.edu>
2017-09-11 12:38:22 -0400

    Week 2 Homework

---
+++ Week2_HW/Assets/Scripts/MusicPlayOnHit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayOnHit : MonoBehaviour {
+
+    AudioSource myAudio;
+
+	// Use this for initialization
+	void Start () {
+        myAudio = GetComponent<AudioSource>();
+	}
+
+    //runs the moment collision happens
+    private void OnCollisionEnter(Collision collision) {
+        // myAudio.Play();
+
+        if (myAudio.isPlaying == false) {
+            myAudio.Play();
+        }
+
+        if (myAudio.isPlaying) {
+            Debug.Log("Audio is playing");
+        }
+    }
+
+    //lasts during collision
+    void onCollisionStay(Collision collision) {
+        Debug.Log("stay");
+    }
+
+}
